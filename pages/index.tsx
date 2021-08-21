cbecf2cb1fbaf31e64d9d48e81a3b6382ce72100
Carlos Henrique Camilo <carloscamilocontato@gmail.com>
2021-08-21 16:15:12 -0300

    i18n init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,23 @@
-import styled from "styled-components";
 import Head from "next/head";
 
 // Components
 import MainHeader from "./../src/components/Header";
 
-export default function Home() {
+export function getStaticProps({ locale }) {
+  return {
+    props: {
+      locale,
+    },
+  };
+}
+
+export default function Home({ locale }) {
   return (
     <>
       <Head>
         <title>Fabrica Logs</title>
       </Head>
+      <h2>{locale}</h2>
       <MainHeader />
     </>
   );
