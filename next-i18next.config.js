8392d4be5acdfcf112cd52f5275ea60f745a0d62
Carlos Camilo <carloscamilocontato@gmail.com>
2021-08-26 12:03:15 -0300

    tentative #20 i18n fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   i18n: {
     locales: ["en", "pt"],
     defaultLocale: "en",
-    localePath: path.resolve("./public/locales")
+    localePath: path.resolve("./static/locales")
   },
 };
