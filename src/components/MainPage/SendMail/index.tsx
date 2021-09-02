b81bef5fd6143760bdefdcc4b2ac51856922479e
Carlos Henrique Camilo <carloscamilocontato@gmail.com>
2021-09-02 12:57:29 -0300

    Multi lang added to email inputs

--- src/components/MainPage/SendMail/index.tsx
+++ src/components/MainPage/SendMail/index.tsx
@@ -1,12 +1,15 @@
 import { Container } from "./styles";
 import Form from "./Form";
 import { Title } from "@components/MainPage/Communs/Title";
+import { useTranslation } from "next-i18next";
 
 function SendMail() {
+  const { t } = useTranslation();
+  
   return (
     <>
       <Container className="email">
-        <Title>Contact me</Title>
+        <Title>{t("email-inputs:Contact me")}</Title>
         <Form />
       </Container>
     </>
