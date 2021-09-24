556db12291876c33957a920fabfd0a8e46378173
Carlos Camilo <carloscamilocontato@gmail.com>
2021-09-24 15:09:34 -0300

    Text corrections and animated placeholder fix

--- src/components/AboutPage/Bio/index.tsx
+++ src/components/AboutPage/Bio/index.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from "react-i18next";
 import { scroller } from "react-scroll";
 
 import Hello from "./Hello";
-import { Container, TextMarkup, EmailButton } from "./styles";
+import { Container, TextMarkup, EmailButton, CvDownloadButton, ButtonsContainer } from "./styles";
 
 function Bio() {
   const { t } = useTranslation();
@@ -11,8 +11,7 @@ function Bio() {
       <Hello />
       <Container>
         <p>
-          {t("aboutme:My name is")} (a.k.a. <TextMarkup>Kalogs</TextMarkup>),{" "}
-          {t("aboutme:I'm based...")}{" "}
+          {t("aboutme:My name is")}. {t("aboutme:I'm based...")}{" "}
           <TextMarkup
             style={{ cursor: "pointer" }}
             onClick={() =>
@@ -23,24 +22,27 @@ function Bio() {
               })
             }
           >
-            {t("aboutme:my knowledge")}
+            {t("aboutme:things that I know")}
           </TextMarkup>
           .
         </p>
         <p>{t("aboutme:I'm focused")}</p>
         <p>{t("aboutme:I also enjoy")}</p>
         <p>{t("aboutme:When I'm not coding")}</p>
-        <EmailButton
-          onClick={() =>
-            scroller.scrollTo("email", {
-              duration: 1000,
-              delay: 0.025,
-              smooth: "easeInOutQuart",
-            })
-          }
-        >
-          Let's talk!
-        </EmailButton>
+        <ButtonsContainer>
+          <CvDownloadButton>Download CV</CvDownloadButton>
+          <EmailButton
+            onClick={() =>
+              scroller.scrollTo("email", {
+                duration: 1000,
+                delay: 0.025,
+                smooth: "easeInOutQuart",
+              })
+            }
+          >
+            Let's talk!
+          </EmailButton>
+        </ButtonsContainer>
       </Container>
     </>
   );
