96667f0e5ea2b7beb15ceaec65250c24d0d38675
Carlos Camilo <carloscamilocontato@gmail.com>
2021-09-27 14:42:00 -0300

    Opacity animation

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,8 +3,14 @@ import { GetServerSideProps } from "next";
 
 import AboutPage from "@components/AboutPage";
 
+import { motion } from "framer-motion";
+
 export default function Projects() {
-  return <AboutPage />;
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+      <AboutPage />
+    </motion.div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
